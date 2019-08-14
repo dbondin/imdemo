75d20c3c0b66ed246058130b262ca07c31f8a3e3
Dmitry Bondin <dbondin@ya.ru>
2019-08-14 18:16:08 +0300

    remove previous draft

--- draft/msg/repos/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.msg.repos;
-
-import com.msg.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
