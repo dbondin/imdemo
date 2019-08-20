66ebc74a136c364bd87e02588f8064a51d833e08
Nimda226 <50245519+Nimda226@users.noreply.github.com>
2019-08-20 13:44:52 +0400

    New RESTfull methods

--- imdemo-server/src/main/java/ru/stm/imdemo/server/controller/MainController.java
+++ imdemo-server/src/main/java/ru/stm/imdemo/server/controller/MainController.java
@@ -1,59 +1,85 @@
 package ru.stm.imdemo.server.controller;
 
-import ru.stm.imdemo.server.domain.Message;
-import ru.stm.imdemo.server.domain.User;
-import ru.stm.imdemo.server.repos.MessageRepo;
+    import org.springframework.beans.BeanUtils;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.web.bind.annotation.*;
+    import ru.stm.imdemo.server.domain.Message;
+    import ru.stm.imdemo.server.repos.MessageRepo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+@RestController
+        @RequestMapping("/")
+        public class MainController {
+            private MessageRepo messageRepo;
 
-import java.util.Map;
+        @Autowired
+        public MainController(MessageRepo messageRepo){
+            this.messageRepo = messageRepo;
+            }
+        @GetMapping
+       public Iterable<Message> list(){
+            return messageRepo.findAll();
+        }
 
-@Controller
-public class MainController {
-    @Autowired
-    private MessageRepo messageRepo;
+        @GetMapping({"id"})
+         public Message getOneById(@PathVariable("id") Message message){
+            return message;
+        }
 
-    //Приветствие
-    @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
-        return "greeting";
+        @PostMapping
+        public Message create(@RequestBody Message message){
+            return messageRepo.save(message);
+        }
+
+        @PutMapping({"id"})
+        public Message update(@PathVariable("id") Message messageFromDb,
+                              @RequestBody Message message){
+            BeanUtils.copyProperties(message, messageFromDb, "id");
+            return messageRepo.save(messageFromDb);
+        }
+
+        @DeleteMapping({"id"})
+        public void delete(@PathVariable("id") Message message){
+            messageRepo.delete(message);
+        }
+
     }
+//
+//    //Приветствие
+//    @GetMapping("/")
+//    public String greeting(Map<String, Object> model) {
+//        return "greeting";
+//    }
 
     /*Возвращает главную страницу и все сообщения на ней
       Так же здесь реализован метод "filter" т.е. поиск по тегу
      */
-    @GetMapping("/main")
-    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
-        Iterable<Message> messages = messageRepo.findAll();
+//    @GetMapping("/")
+//    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
+//        Iterable<Message> messages = messageRepo.findAll();
 //FIXME
 //        if (filter != null && !filter.isEmpty()) {
 //            messages = messageRepo.findByTag(filter);
 //        } else {
-            messages = messageRepo.findAll();
+//            messages = messageRepo.findAll();
 //        }
-        model.addAttribute("messages", messages);
-        model.addAttribute("filter", filter);
-        return "main";
-    }
+//        model.addAttribute("messages", messages);
+//        model.addAttribute("filter", filter);
+//        return "/";
+//    }
 
     //Метод добавления сообщений
-    @PostMapping("/main")
-    public String add(  @AuthenticationPrincipal User user,
-                        @RequestParam String text,
-                        @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message();
-        message.setFromUser(user);
-        message.setText(text);
-        //FIXME message.setToUser(user);
-        messageRepo.save(message);
-        Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-}
+//    @PostMapping("/")
+//    public String add(  @AuthenticationPrincipal User user,
+//                        @RequestParam String text,
+//                        @RequestParam String tag, Map<String, Object> model){
+//        Message message = new Message();
+//        message.setFromUser(user);
+//        message.setText(text);
+//        //FIXME message.setToUser(user);
+//        messageRepo.save(message);
+//        Iterable<Message> messages = messageRepo.findAll();
+//        model.put("messages", messages);
+//        return "/";
+//    }
+//}
+
